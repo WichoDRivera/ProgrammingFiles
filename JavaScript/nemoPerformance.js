7101a2e764730b7442457f7bf7019599d5fef4fe
Luis Daniel Rivera Salinas <56170272+WichoDRivera@users.noreply.github.com>
2020-05-17 17:43:40 -0500

    Create nemoPerformance.js

---
+++ JavaScript/nemoPerformance.js
@@ -0,0 +1,22 @@
+//To check performance, we use performance.now
+//To check how long does it takes to run
+
+const {performance} = require('perf_hooks');
+
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+
+const large = new Array(100).fill('nemo');
+
+function findNemo2(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!');
+    }
+  }
+  let t1 = performance.now();
+  console.log("Call to find Nemo took " + (t1 - t0) + " milliseconds.");
+}
+
+findNemo2(large)
