84f2e36d9580185c7ae38a0bccc6e8da7746e030
Luis Daniel Rivera Salinas <56170272+WichoDRivera@users.noreply.github.com>
2020-06-16 19:08:52 -0500

    Update arrayMethods.py

--- Python 3/Data_Structures/Arrays/arrayMethods.py
+++ Python 3/Data_Structures/Arrays/arrayMethods.py
@@ -1,1 +1,22 @@
-print("Not yet !!")
+# Arrays : Organize items sequentially
+
+strings = ['a','b','c','d']
+# 4 per each item = 16 bytes of storage used
+
+print(strings[2])
+
+# Push: Add item at the end 
+strings.append('e')      # O(1)
+
+# Pop: Delete item at the end
+strings.pop() 
+strings.pop()            # O(1)
+
+# Addfirstelement: Add element at the beggining (position 0)
+strings.insert(0,'x')    #O(n)
+
+# Splice: Divide the array
+# Insert: its used to insert in the desire position
+strings.insert(2,'alien')   #O(n)
+
+print(strings)
