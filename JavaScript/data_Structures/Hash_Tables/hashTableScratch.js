e8ba81803ee6f0a30facb685fb6f28126f4e738d
Luis Daniel Rivera Salinas <56170272+WichoDRivera@users.noreply.github.com>
2020-08-04 17:39:49 -0500

    Update hashTableScratch.js

--- JavaScript/data_Structures/Hash_Tables/hashTableScratch.js
+++ JavaScript/data_Structures/Hash_Tables/hashTableScratch.js
@@ -104,6 +104,30 @@ class HashTable2 {
     return keyArr;
   }
 
+  // keys with collision detection
+   keysNoColl() {
+    if (!this.data.length) {
+      return undefined
+    }
+    let result = []
+    // loop through all the elements
+    for (let i = 0; i < this.data.length; i++) {
+        // if it's not an empty memory cell
+        if (this.data[i] && this.data[i].length) {
+          // but also loop through all the potential collisions
+          if (this.data.length > 1) {
+            for (let j = 0; j < this.data[i].length; j++) {
+              result.push(this.data[i][j][0])
+            }
+          } else {
+            result.push(this.data[i][0])
+          } 
+        }
+    }
+    return result; 
+  }
+
+
 }
 
 const myHashTable2 = new HashTable2(20);
