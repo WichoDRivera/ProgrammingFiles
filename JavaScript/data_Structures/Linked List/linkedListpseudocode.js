8ad9d030fa670b9d4c6d26370f04e4cb5584a4ef
Luis Daniel Rivera Salinas <56170272+WichoDRivera@users.noreply.github.com>
2020-08-06 17:03:47 -0500

    Update linkedListpseudocode.js

--- JavaScript/data_Structures/Linked List/linkedListpseudocode.js
+++ JavaScript/data_Structures/Linked List/linkedListpseudocode.js
@@ -1,3 +1,4 @@
+/*
 Linked List Pseaudocode
 
 const basket = ['apples', 'grapes', 
@@ -9,3 +10,14 @@ apples
 8947 (Memory) --> grapes
                   8742   -->  pears
                               372   --> null 
+*/
+
+let obj1 = {a: true}
+let obj2 = obj1; // ---> pointer to obj1, its the same location in memory as obj1
+
+obj1.a = 'booya';
+delete obj1
+obj2 = 'hello'
+
+console.log('1' , obj1);
+console.log('2. ', obj2);
