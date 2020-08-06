06ee430905ec00da994bed9a0aefd326d2984bc2
Luis Daniel Rivera Salinas <56170272+WichoDRivera@users.noreply.github.com>
2020-08-05 21:35:40 -0500

    Create linkedListpseudocode.js

---
+++ JavaScript/data_Structures/Linked List/linkedListpseudocode.js
@@ -0,0 +1,11 @@
+Linked List Pseaudocode
+
+const basket = ['apples', 'grapes', 
+'pears'];
+
+linked list: apples --> grapes --> pears
+
+apples
+8947 (Memory) --> grapes
+                  8742   -->  pears
+                              372   --> null 
