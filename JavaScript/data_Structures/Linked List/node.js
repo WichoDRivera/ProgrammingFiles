b7418972cfd307a26c2cbc81de5e06e0b7382d0b
Luis Daniel Rivera Salinas <56170272+WichoDRivera@users.noreply.github.com>
2020-08-11 13:51:38 -0500

    Create node.js

---
+++ JavaScript/data_Structures/Linked List/node.js
@@ -0,0 +1,11 @@
+/*
+* Node class to insert or delete, this avoid duplicated code
+* Already built in Linked List Code
+*/
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
