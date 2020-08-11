75cba46c29d60f767404c2004dc9e14e3a1c4d36
Luis Daniel Rivera Salinas <56170272+WichoDRivera@users.noreply.github.com>
2020-08-11 15:49:26 -0500

    Update linkedListScratch.js

--- JavaScript/data_Structures/Linked List/linkedListScratch.js
+++ JavaScript/data_Structures/Linked List/linkedListScratch.js
@@ -1,5 +1,14 @@
 // add a method prepend() to the linked list that adds a node to the beginning of the list
 
+// 1 --> 10 --> 99 --> 5 --> 16
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -10,31 +19,67 @@ class LinkedList {
     this.length = 1;
   }
   append(value) {
-    const newNode = {
-      value: value,
-      next: null
-    }
-    console.log(newNode)
+    let newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
     return this;
   }
   prepend(value) {
-    const newNode = {
-      value: value,
-      next: null
-    }
+    let newNode = new Node(value);
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
     return this;
   }
+
+  insert(index, value){
+  // Check parameters
+    if(index === 0){
+      this.prepend(value);
+      return this.printList();
+    } else if(index >= this.length){
+      this.append(value);
+      return this.printList();
+    }
+
+    // Insert code 
+    let newNode = new Node(value);
+    const leader = this.traversToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  traversToIndex(index){
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index){
+       //console.log("Node :", counter, currentNode)
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  printList(){
+    const array = [];
+    let currentNode = this.head;
+    while(currentNode !== null){
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
 }
 
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
-myLinkedList.append(16);myLinkedList.prepend(1)
+myLinkedList.append(16);myLinkedList.prepend(1);
+myLinkedList.insert(2,99);
+
 
 
 
